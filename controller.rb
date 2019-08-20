9eda4a9f72566e8680c3735c9c504a77d8912918
Joe Cooper <josephcooper3@gmail.com>
2019-08-20 15:21:12 +0100

    complete MVP

--- controller.rb
+++ controller.rb
@@ -5,15 +5,15 @@ require_relative('./models/film')
 also_reload('./models/*')
 
 get '/films' do
-  films = Film.all()
-  @film_titles = []
-  for film in films
-    @film_titles << film.title()
-  end
+  @films = Film.all()
+  # @film_titles = []
+  # for film in films
+  #   @film_titles << film.title()
+  # end
   erb(:films)
 end
 
-get '/:id' do
+get '/films/:id' do
   @film_details = Film.find(params[:id])[0]
   erb(:film_details)
 end
